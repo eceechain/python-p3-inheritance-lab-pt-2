abaa2565c0110d68c6d3e2ffec88ae7717ef2970
ecee <you@example.com>
2023-11-27 14:14:36 +0300

    Done with Assignment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,18 @@
 class Student:
-    pass
+
+        def hello(self):
+            print("Hey there! I'm so excited to learn stuff.")
+        
+        def raise_hand(self):
+            print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    
+        def hello(self):
+            super().hello()
+            print("I'm so glad to be here with you all!")
+    
+        def raise_hand(self):
+            for i in range(10):
+                super().raise_hand()
